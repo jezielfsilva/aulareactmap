90dd8ab6d14159d84f098abb6efe012a4533f080
vai na web <vainawebcpx@gmail.com>
2019-12-04 17:34:19 -0300

    novs arquivos criados

---
+++ src/components/listaarray.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import App from '../App';
+import '../App.css';
+
+class list extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            estudantes: [
+                {
+                    nome: 'Breno',
+                    polo: 'prazeres',
+                    idade: 18
+                },
+                {
+                    nome: 'Matheus', 
+                    polo: 'prazeres',
+                    idade: 23
+                },
+                {
+                    nome: 'Kamile',
+                    polo: 'prazeres',
+                    idade: 16
+                },
+                {
+                    nome: 'Jeziel',
+                    polo: 'prazeres',
+                    idade: 33
+                },
+                {
+                    nome: 'Cícero',
+                    polo: 'prazeres',
+                    idade: 24
+                },
+                {
+                    nome: 'Cassio',
+                    polo: 'prazeres',
+                    idade: 22
+                },
+                {
+                    nome: 'Douglas',
+                    polo: 'prazeres',
+                    idade: 24
+                },
+                {
+                    nome: 'Pricila',
+                    polo: 'prazeres',
+                    idade: 28
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+           <div>
+               {this.state.estudantes.map((item, index) => {
+                   return (
+                       <div className='boxcard' key={index}>
+                           <h1>{item.nome}</h1>
+                           <h2>{item.polo}</h2>
+                           <h2>{item.idade}</h2>
+                       </div>
+                   );
+               })
+            }
+           </div>
+        )
+    }
+}
+
+export default list;
